925428c89b0f3416f8d9dc1957156d0c46a58b1c
Delia Constantino <dconstan92@gmail.com>
2020-11-03 23:51:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+require 'pry'
+
+class School
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    roster[grade] = []
+    roster[grade] << name
+  end
+
+  # def add_student(name, grade)
+  #   roster[grade].push(name)
+  # end
+
+
+
+end
