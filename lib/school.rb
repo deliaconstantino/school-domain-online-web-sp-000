0233369ced7969b1571b6013401aea1cd67bad9c
Delia Constantino <dconstan92@gmail.com>
2020-11-04 00:00:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,9 +10,9 @@ class School
 
 
   def add_student(name, grade)
-    if @roster.has_key(grade)?
+    if @roster.has_key?(grade)
       roster[grade] << name
-    end
+    else
       @roster[grade] = []
       @roster[grade] << name
     end
